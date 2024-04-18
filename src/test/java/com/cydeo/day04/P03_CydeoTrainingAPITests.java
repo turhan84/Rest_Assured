c599c235b9ba5c12a3f66a590a10806f2c86de89
Turhan Ismailov <turhan.ismailov@gmail.com>
2024-04-18 17:33:05 +0100

    Day 4

--- src/test/java/com/cydeo/day04/P03_CydeoTrainingAPITests.java
+++ src/test/java/com/cydeo/day04/P03_CydeoTrainingAPITests.java
@@ -1,6 +1,38 @@
 package com.cydeo.day04;
 
 import com.cydeo.utilities.CydeoTestBase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class P03_CydeoTrainingAPITests extends CydeoTestBase {
+
+      /*
+    Given accept type is application/json
+    And path param is 2
+    When user send request /student/{id}
+    Then status code should be 200
+    And content type is application/json;charset=UTF-8
+    And Date header is exist
+    And Server header is envoy
+    And verify following
+                firstName Mark
+                batch 13
+                major math
+                emailAddress mark@email.com
+                companyName Cydeo
+                street 777 5th Ave
+                zipCode 33222
+    */
+
+    @DisplayName("GET /student/2")
+    @Test
+    public void test1(){
+
+
+
+
+
+    }
+
+
 }
