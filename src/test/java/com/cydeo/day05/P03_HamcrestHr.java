bc2161c3b08c125ccb394019707b35021af650c3
Turhan Ismailov <turhan.ismailov@gmail.com>
2024-04-22 11:15:20 +0100

    Day 5

--- src/test/java/com/cydeo/day05/P03_HamcrestHr.java
+++ src/test/java/com/cydeo/day05/P03_HamcrestHr.java
@@ -2,6 +2,7 @@ package com.cydeo.day05;
 
 import com.cydeo.utilities.HrTestBase;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -33,24 +34,24 @@ public class P03_HamcrestHr extends HrTestBase {
 
     @DisplayName("GET employees IT PROG with hamcrest")
     @Test
-    public void test1(){
+    public void test1() {
         //expected firstnames
-        List<String> names = Arrays.asList("Alexander","Bruce","David","Valli","Diana");
+        List<String> names = Arrays.asList("Alexander", "Bruce", "David", "Valli", "Diana");
 
         given()
                 .accept(ContentType.JSON)
-                .queryParam("q","{\"job_id\":\"IT_PROG\"}")
-        .when()
+                .queryParam("q", "{\"job_id\":\"IT_PROG\"}")
+                .when()
                 .get("/employees")
-        .then()
+                .then()
                 .statusCode(200)
                 .contentType("application/json")
                 .and()
-                .body("items.manager_id",everyItem(notNullValue()))
-                .body("items.job_id",everyItem(equalTo("IT_PROG")))
-                .body("items.salary",everyItem(greaterThan(3000)))
-                .body("items.first_name",equalTo(names))
-                .body("items.email",containsInAnyOrder("DLORENTZ","DAUSTIN","AHUNOLD","BERNST","VPATABAL"));
+                .body("items.manager_id", everyItem(notNullValue()))
+                .body("items.job_id", everyItem(equalTo("IT_PROG")))
+                .body("items.salary", everyItem(greaterThan(3000)))
+                .body("items.first_name", equalTo(names))
+                .body("items.email", containsInAnyOrder("DLORENTZ", "DAUSTIN", "AHUNOLD", "BERNST", "VPATABAL"));
 
     }
 
@@ -75,12 +76,33 @@ public class P03_HamcrestHr extends HrTestBase {
                .
     */
 
+    @DisplayName("GET All regions with hamcrest")
     @Test
-    public void test2(){
+    public void test2() {
+        JsonPath jsonPath = given()
+                .when()
+                .get("/regions")
+                .then()
+                .statusCode(200)
+                .contentType("application/json")
+                .header("Date", notNullValue())
+                .body("items[0].region_name", equalTo("Europe"))
+                .body("items[0].region_id", equalTo(1))
+                .body("items", hasSize(4))
+                .body("items.region_name", everyItem(notNullValue()))
+                .body("items.region_name", containsInRelativeOrder("Europe", "Americas", "Asia", "Middle East and Africa"))
+                .body("items.region_id", containsInRelativeOrder(1, 2, 3, 4))
+                .extract().jsonPath();
 
+
+        List<String> allRegionsNames = jsonPath.getList("items.region_name");
+        System.out.println("allRegionsNames = " + allRegionsNames);
+
+        //get regions names from ui or database
+
+        //compare
 
     }
-
 
 
 }
