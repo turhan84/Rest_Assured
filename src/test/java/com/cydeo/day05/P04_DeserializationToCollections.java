7991ec6dc69f49bb15ae69f9b25f6ee03144fe60
Turhan Ismailov <turhan.ismailov@gmail.com>
2024-04-22 11:37:30 +0100

    Day 5

--- src/test/java/com/cydeo/day05/P04_DeserializationToCollections.java
+++ src/test/java/com/cydeo/day05/P04_DeserializationToCollections.java
@@ -2,7 +2,9 @@ package com.cydeo.day05;
 
 import com.cydeo.utilities.SpartanTestBase;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -53,7 +55,68 @@ public class P04_DeserializationToCollections extends SpartanTestBase {
         Map<String,Object> spartanMap = response.as(Map.class);
         System.out.println("spartanMap = " + spartanMap);
 
+        int id = (int) spartanMap.get("id");
+        String name = (String) spartanMap.get("name");
+
+        System.out.println("id = " + id);
+        System.out.println("name = " + name);
+
+        //Solution 2 --> JsonPath
+        JsonPath jsonPath = response.jsonPath();
+
+        Map<String,Object> jsonPathMap =jsonPath.getMap("");
+        System.out.println("jsonPathMap = " + jsonPathMap);
+
+        int idJson = (int) jsonPathMap.get("id");
+
+        System.out.println("idJson = " + idJson);
+
     }
 
+    @DisplayName("GET All Spartans with Java Collections")
+    @Test
+    public void test2() {
 
-}
+        Response response = given()
+                .accept(ContentType.JSON)
+                .when()
+                .get("/api/spartans/")
+                .then()
+                .statusCode(200)
+                .contentType("application/json")
+                .extract().response();
+
+        //Approach 1 -->With response object.
+        List<Map<String, Object>> spartanList = response.as(List.class);
+
+//        for (Map<String, Object> map : spartanList) {
+//            System.out.println("spartanMap = " + map);
+//        }
+
+        //how to find first spartan info
+        System.out.println("spartanList.get(0) = " + spartanList.get(0));
+
+        //how to find first spartan name
+        System.out.println("spartanList.get(0).get(\"name\") = " + spartanList.get(0).get("name"));
+
+        //how to find first spartan id
+        System.out.println("spartanList.get(0).get(\"id\") = " + spartanList.get(0).get("id"));
+
+
+        //Approach 2 -- JSONPATH
+        JsonPath jsonPath = response.jsonPath();
+        List<Map<String,Object>> listSpartan = jsonPath.getList("");
+
+        System.out.println("listSpartan = " + listSpartan);
+
+        //how to find first spartan info
+        System.out.println("spartanList.get(0) = " + listSpartan.get(0));
+
+        //how to find first spartan name
+        System.out.println("spartanList.get(0).get(\"name\") = " + listSpartan.get(0).get("name"));
+
+        //how to find first spartan id
+        System.out.println("spartanList.get(0).get(\"id\") = " + listSpartan.get(0).get("id"));
+
+    }
+    }
