1906b72edcec33efb9a70a25f1dba352fce3fc82
Turhan Ismailov <turhan.ismailov@gmail.com>
2024-04-25 12:58:05 +0100

    Day 7 P02_SpartanPOST

--- src/test/java/com/cydeo/day07/P02_SpartanPOST.java
+++ src/test/java/com/cydeo/day07/P02_SpartanPOST.java
@@ -39,8 +39,35 @@ public class P02_SpartanPOST extends SpartanTestBase {
     @Test
     public void test1(){
 
+        String requestBody = "     {\n" +
+                "     \"gender\":\"Male\",\n" +
+                "     \"name\":\"John Doe X\",\n" +
+                "     \"phone\":8877445596\n" +
+                "     }";
 
 
+        JsonPath jsonPath = given().log().body()
+                .accept(ContentType.JSON)//hey api, please send me JSON RESPONSE BODY
+                .and()
+                // .contentType("application/json")
+                .contentType(ContentType.JSON)// hey api, I am sending you JSON REQUEST BODY
+                .body(requestBody)
+                .when()
+                .post("/api/spartans").prettyPeek()
+                .then()
+                .statusCode(201)
+                .contentType("application/json")
+                .body("success", is("A Spartan is Born!"))
+                .extract().jsonPath();
+
+        //request body verification
+        assertEquals("John Doe X", jsonPath.getString("data.name"));
+        assertEquals("Male", jsonPath.getString("data.gender"));
+        assertEquals(8877445596l, jsonPath.getLong("data.phone"));
+
+        //I want to get id out of the response body, to delete or send get request later on
+        int id = jsonPath.getInt("data.id");
+        System.out.println("id = " + id);
 
 
     }
@@ -50,6 +77,39 @@ public class P02_SpartanPOST extends SpartanTestBase {
     @Test
     public void test2(){
 
+        Map<String,Object> requestBodyMap = new LinkedHashMap<>();
+        requestBodyMap.put("name","John Doe");
+        requestBodyMap.put("gender","Male");
+        requestBodyMap.put("phone","8877445596");
+
+        //we created one map, and put the info that we want to send as a JSON REQUEST BODY
+        System.out.println("requestBodyMap = " + requestBodyMap);
+
+        JsonPath jsonPath = given().log().body()
+                .accept(ContentType.JSON)//hey api, please send me JSON RESPONSE BODY
+                .and()
+                // .contentType("application/json")
+                .contentType(ContentType.JSON)// hey api, I am sending you JSON REQUEST BODY
+                .body(requestBodyMap)
+                .when()
+                .post("/api/spartans").prettyPeek()
+                .then()
+                .statusCode(201)
+                .contentType("application/json")
+                .body("success", is("A Spartan is Born!"))
+                .extract().jsonPath();
+
+        //request body verification
+        assertEquals("John Doe X", jsonPath.getString("data.name"));
+        assertEquals("Male", jsonPath.getString("data.gender"));
+        assertEquals(8877445596l, jsonPath.getLong("data.phone"));
+
+        //I want to get id out of the response body, to delete or send get request later on
+        int id = jsonPath.getInt("data.id");
+        System.out.println("id = " + id);
+
+        //can we have SpartanUtil class which is giving as a requestMap with dynamic dummy values ?
+        //using faker library inside ?
 
 
 
@@ -58,6 +118,37 @@ public class P02_SpartanPOST extends SpartanTestBase {
     @DisplayName("POST Spartan with Spartan POJO")
     @Test
     public void test3(){
+        Spartan spartan = new Spartan();
+        spartan.setName("Harold Finch");
+        spartan.setGender("Male");
+        spartan.setPhone(1234567890l);
+
+        System.out.println("spartan = " + spartan);
+
+        JsonPath jsonPath = given().log().body()
+                .accept(ContentType.JSON) //hey api, please send me JSON RESPONSE BODY
+                .and()
+                .contentType(ContentType.JSON) // hey api, I am sending you JSON REQUEST BODY
+                .body(spartan)
+                .when()
+                .post("/api/spartans").prettyPeek()
+                .then()
+                .statusCode(201)
+                .contentType("application/json")
+                .body("success",is("A Spartan is Born!"))
+                .extract().jsonPath();
+
+        //request body verification
+        assertEquals("Harold Finch",jsonPath.getString("data.name"));
+        assertEquals("Male",jsonPath.getString("data.gender"));
+        assertEquals(1234567890l,jsonPath.getLong("data.phone"));
+
+        //I want to get id out of the response body, to delete or send get request later on
+        int id = jsonPath.getInt("data.id");
+        System.out.println("id = " + id);
+
+        //can we have SpartanUtil class which is giving as a requestMap with dynamic dummny values ?
+        //using faker library inside ?
 
 
 
@@ -67,6 +158,53 @@ public class P02_SpartanPOST extends SpartanTestBase {
     @DisplayName("POST Spartan with Spartan POJO and GET same Spartan")
     @Test
     public void test4(){
+        //empty spartan object and we use setters to set some value
+        //values can be from faker library or somewhere else which is changing dynamically
+        Spartan spartanPOST = new Spartan();
+        spartanPOST.setName("Harold Finch");
+        spartanPOST.setGender("Male");
+        spartanPOST.setPhone(1234567890l);
+        spartanPOST.setId(500); //even if we put some id value, it didnt serialized because of the jackson annotation on the class
+
+        System.out.println("spartan = " + spartanPOST);
+
+
+        JsonPath jsonPath = given().log().body()
+                .accept(ContentType.JSON) //hey api, please send me JSON RESPONSE BODY
+                .and()
+                .contentType(ContentType.JSON) // hey api, I am sending you JSON REQUEST BODY
+                .body(spartanPOST) //it will take spartan object and serialize to JSON
+                .when()
+                .post("/api/spartans").prettyPeek()
+                .then()
+                .statusCode(201)
+                .contentType("application/json")
+                .body("success",is("A Spartan is Born!"))
+                .extract().jsonPath();
+
+        //request body verification
+        assertEquals("Harold Finch",jsonPath.getString("data.name"));
+        assertEquals("Male",jsonPath.getString("data.gender"));
+        assertEquals(1234567890l,jsonPath.getLong("data.phone"));
+
+        //I want to get id out of the response body, to delete or send get request later on
+        int id = jsonPath.getInt("data.id");
+        System.out.println("id = " + id);
+
+        //SEND GET REQUEST TO THE SPARTAN THAT IS CREATED THEN DESERIALIZE TO SPARTAN CLASS and compare
+
+        Response response = given()
+                .accept(ContentType.JSON)
+                .pathParam("id", id)
+                .when().get("/api/spartans/{id}")
+                .then().statusCode(200).extract().response();
+        //get json response and deserialize to spartan class
+        Spartan spartanGET = response.as(Spartan.class);
+
+        System.out.println("spartanGET = " + spartanGET);
+
+        //verify names that we sent and get is matching
+        assertEquals(spartanPOST.getName(),spartanGET.getName());
 
 
 
