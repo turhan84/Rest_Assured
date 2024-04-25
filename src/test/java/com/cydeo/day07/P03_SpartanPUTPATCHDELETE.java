05c0f6059c5f532305f9f3ac4561c6cf2cf94591
Turhan Ismailov <turhan.ismailov@gmail.com>
2024-04-25 13:37:16 +0100

    Day 7 P03_SpartanPUTPATCHDELETE

--- src/test/java/com/cydeo/day07/P03_SpartanPUTPATCHDELETE.java
+++ src/test/java/com/cydeo/day07/P03_SpartanPUTPATCHDELETE.java
@@ -15,6 +15,24 @@ public class P03_SpartanPUTPATCHDELETE extends SpartanTestBase {
     @DisplayName("PUT Spartan with Map")
     @Test
     public void test1(){
+        //we can provide json request body with map,pojo,string all is valid here too.
+
+        Map<String,Object> requestBodyMap = new LinkedHashMap<>();
+        requestBodyMap.put("name","John Doe PUT");
+        requestBodyMap.put("gender","Male");
+        requestBodyMap.put("phone","8877445596");
+
+        //PUT will update existing record so we choose one the existing ID, make sure it exist in your IP address
+        int id = 113;
+
+        given()
+                .contentType(ContentType.JSON)
+                .pathParam("id",id)
+                .body(requestBodyMap)
+                .when().put("/api/spartans/{id}")
+                .then().statusCode(204);
+
+
 
     }
 
@@ -23,6 +41,23 @@ public class P03_SpartanPUTPATCHDELETE extends SpartanTestBase {
     @Test
     public void test2(){
 
+        //we can provide json request body with map,pojo,string all is valid here too.
+
+        Map<String,Object> requestBodyMap = new LinkedHashMap<>();
+        requestBodyMap.put("name","John Doe PATCH");
+
+
+        //PUT will update existing record so we choose one the existing ID, make sure it exist in your IP address
+        int id = 113;
+
+        given()
+                .contentType(ContentType.JSON)
+                .pathParam("id",id)
+                .body(requestBodyMap)
+                .when()
+                .patch("/api/spartans/{id}")
+                .then()
+                .statusCode(204);
 
 
     }
@@ -32,5 +67,18 @@ public class P03_SpartanPUTPATCHDELETE extends SpartanTestBase {
     public void test3(){
 
 
+        //we can delete one id only one time, so it will give 204 only for the first execution
+        int id = 113;
+
+        given()
+                .pathParam("id",id)
+                .when().delete("/api/spartans/{id}")
+                .then().statusCode(204);
+
+        //after deleted when we send get request to id that we deleted, it needs to give 404
+        given()
+                .pathParam("id",id)
+                .when().get("/api/spartans/{id}")
+                .then().statusCode(404);
     }
 }
